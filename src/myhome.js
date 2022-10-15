93ce3d52a5145b1a137f53427ab4409afaf5417d
mio256 <reiya@woorth.io>
2022-10-15 18:01:40 +0900

    :recycle: use process.env in myhome.js

--- src/myhome.js
+++ src/myhome.js
@@ -2,11 +2,11 @@ import { TwitterApi } from 'twitter-api-v2';
 
 // Instantiate with desired auth type (here's Bearer v2 auth)
 const twitterClient = new TwitterApi({
-  bearerToken: '*',
-  appKey: '*',
-  appSecret: '*',
-  accessToken: '*',
-  accessSecret: '*',
+  bearerToken: process.env.BEARER_TOKEN,
+  appKey: process.env.CONSUMER_KEY,
+  appSecret: process.env.CONSUMER_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
 });
 
 const homeTimeline = await twitterClient.v2.homeTimeline({ exclude: 'replies' })
