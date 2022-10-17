58a4291c621a2657a43d364124e5e56952370fe3
sny430 <shinya430.g@gmail.com>
2022-10-17 10:36:01 +0900

    :art: the first tentative design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    
+    <div className="App">
+
+      <h1>TwiCordGram(仮)</h1>
+
+      <div className="example2">
+        <div>
+          ここに新規投稿作成UIを置く<br></br>
+          TwitterとInstagramを切り替えられるようにする
+        </div>
+        <div>ここにTwitterのTLを表示</div>
+        <div>ここにInstagramのTLを表示</div>
+      </div>
+
+    </div>
+    
   );
 }
 
