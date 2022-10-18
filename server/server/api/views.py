36ad1515f529ec93bd0a32a4b69d8fb1b1db34ae
MitsukuniH <hmitsuku3292@gmail.com>
2022-10-18 13:31:25 +0900

    :sparkles:Display Twitter-Timeline

--- server/server/api/views.py
+++ server/server/api/views.py
@@ -1,3 +1,30 @@
+import json
+import os
+import tweepy
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home_json(request):
+    """
+    Display Twitter-Timeline.
+    """
+    client = tweepy.Client(
+    consumer_key='***',
+    consumer_secret='***',
+    access_token='***',
+    access_token_secret='***'
+    )
+    
+    if request.method == 'GET':
+        response = client.get_home_timeline()
+        timeline = []
+        for tweets in response:
+            string = []
+            for tweet in tweets:
+                string.append(str(tweet))
+            timeline.append(string)
+
+        data = {
+            "timeline": timeline
+        }
+        return JsonResponse(data)
