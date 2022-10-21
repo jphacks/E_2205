2de894c93ac79ca9cc13fd1eba2527d0b91d3b89
yu0119 <yuuyiteng0@gmail.com>
2022-10-21 16:31:12 +0900

    :sparkles: init textbox of create post UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,129 +2,163 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState} from "react";
+
+type TodoType = {
+  id: number;
+  todo: string;
+  isDone: boolean;
+};
+
+
+
+const App: React.FC = () => {
+  const [text, setText] = useState<string>("");
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setText(e.target.value);
+  };
+  const handleClick = (): void => {
+    setTodos([
+      ...todos,
+      { id: todos.length++, todo: text, isDone: false }
+    ]);
+  };
+
   return (
-    
-    <div className="App">
+    <>
+      
+      <div className="App">
 
-      <div className="left_element">
+<div className="left_element">
 
-        <h1>TwiCordGram(仮)</h1>
+  <h1>TwiCordGram(仮)</h1>
 
-        <div className='create_post'>
-          Twitter新規投稿作成UI
-        </div>
+  <div className='create_post'>
+    <input
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}
+        type="text"
+      />
+      <button onClick={(): void => handleClick()}>tweet</button>
+      {todos.map((number) => (
+        <p key={number.id}>{number.todo}</p>
 
-        <div className='create_post'>
-          Instagram新規投稿作成UI
-        </div>
+      ))}
+  </div>
 
-      </div>
+  <div className='create_post'>
+    Instagram新規投稿作成UI
+  </div>
 
-      <div className="center_element">
-        <div className="subtitle"><p>Twitter</p></div>
+</div>
 
-        <div className="timeline">
+<div className="center_element">
+  <div className="subtitle"><p>Twitter</p></div>
 
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
+  <div className="timeline">
 
-        </div>
-      </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
 
-      <div className="center_element">
-        <div className="subtitle"><p>Instagram</p></div>
-        <div className="timeline">
+  </div>
+</div>
 
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
-        <div className="post">
-          <div className="username"><p>user</p></div>
-          <p>text</p>
-        </div>
+<div className="center_element">
+  <div className="subtitle"><p>Instagram</p></div>
+  <div className="timeline">
 
-        </div>
-      </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
+  <div className="post">
+    <div className="username"><p>user</p></div>
+    <p>text</p>
+  </div>
 
-      <div className="right_element">
-        設定アイコンとかを置く(余裕があれば)
-      </div>
+  </div>
+</div>
 
-    </div>
-    
+<div className="right_element">
+  設定アイコンとかを置く(余裕があれば)
+</div>
+
+</div>
+
+    </>
   );
-}
-
+};
 export default App;
+
+
