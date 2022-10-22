2568ff92d31d1b4f010a8098e71c03a4d8dbda8a
sny430 <shinya430.g@gmail.com>
2022-10-22 10:03:17 +0900

    :recycle: textbox function separated

---
+++ client/src/TextBox.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+
+import {useState} from "react";
+
+type TodoType = {
+  id: number;
+  todo: string;
+  isDone: boolean;
+};
+
+const TextBox: React.FC = () => {
+  const [text, setText] = useState<string>("");
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setText(e.target.value);
+  };
+  const handleClick = (): void => {
+    setTodos([
+      ...todos,
+      { id: todos.length++, todo: text, isDone: false }
+    ]);
+  };
+
+  return (
+    <div className="TextBox">
+        <input
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}
+            type="text"
+        />
+        <button onClick={(): void => handleClick()}>New Post</button>
+        {
+            todos.map((number) => (
+            <p key={number.id}>
+                {number.todo}
+            </p>
+        ))}
+    </div>
+  );
+};
+
+export default TextBox;
+
+
